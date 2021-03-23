f1fec8d45bbde9cf8552c99934a517dd18dd3156
Otaviano Pires <otavianopires@gmail.com>
2021-03-23 08:48:41 -0400

    Redirect Subscribers from Dashboard

--- wp-handy-snippets.php
+++ wp-handy-snippets.php
@@ -11,3 +11,17 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Redirect Subscribers from Dashboard
+ */
+function redirect_subscribers_from_dashboard() {
+    if ( is_admin() && current_user_can( 'subscriber' ) && $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+        global $wp_query;
+        $wp_query->set_404();
+        status_header(404);
+        wp_safe_redirect( home_url('/404') );
+        exit;
+    }
+}
+add_action( 'admin_init', 'redirect_subscribers_from_dashboard', 1 );
