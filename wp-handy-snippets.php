1f4c68e319f65a74c03cf8cdd6c8f42ea75a226b
Otaviano Pires <otavianopires@gmail.com>
2021-03-23 08:48:23 -0400

    Added plugin header

---
+++ wp-handy-snippets.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       WP Handy Snippets
+ * Plugin URI:        https://github.com/otavianopires/wp-protected-attachments
+ * Description:       Handy snippets for WordPress Themes or Plugins
+ * Version:           0.1
+ * Author:            Otaviano Pires Amancio
+ * Author URI:        https://github.com/otavianopires
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
